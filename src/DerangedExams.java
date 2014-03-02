ae7406f2c032dbf236d9fe112ee6afa26b52fb42
Chris Chapman <chrischapman900@gmail.com>
2014-03-01 22:20:14 -0500

    It's working
    I'm not sure how yet, but it is. I plugged in a function from a Puget
    Sound paper and it seems to be working for all test cases.
    
    Still need to add enough comments and figure out exactly how it works.

--- src/DerangedExams.java
+++ src/DerangedExams.java
@@ -5,30 +5,181 @@ public class DerangedExams {
 
 	public static void main(String[] args) {
 		
-		//test values
-		int n = 7;
-		int k = 3;
+		int n = 4;
+		int k = 1;
 		
-		System.out.println(partialDerange(n, n-k));
+		long puget = pugetSoundFunction(n, k, true);
+		
+		System.out.println("Puget: " + puget);
+	}
+	
+	public static long pugetSoundFunction(int n, int k, boolean explain) {
+		
+		/*
+		 * Explaination found on 
+		 * http://mathcs.pugetsound.edu/~mspivey/DT.pdf
+		 * 
+		 * Theorem 4.
+		 * 
+		 * 
+		 * 					_k_			/ k \	
+		 * 		S n,k =	 	\	(-1)^i	|	|	(n - i)!
+		 *					/__			\ i /	  
+		 *					i=0
+		 */
+		
+		
+		long ans = 0;
+		
+		if (explain) {
+			System.out.println("With " + factorial(n) + " different ways to answer all of the " + n + " questions,");
+			System.out.println("and starting with already having the first " + k + " wrong:");
+			System.out.println();
+		}
+		
+		for (int i = 0; i <= k; i++) {
+			int neg1ToTheK = (int)Math.pow(-1, i);
+			long combKI = combination(k, i);
+			long nSubIFact = factorial(n-i);
+			
+			long inner = neg1ToTheK * combKI * nSubIFact;
+			
+			if (explain) {
+				System.out.println("With only " + (i+1) + " questions answered,");
+				System.out.println("Inner " + i + "= " + inner);
+			}
+			
+			ans = ans + (inner);
+		}
+		
+		return ans;
+	}
+	
+	
+	
+	
+	
+/* **************************************************
+ * 					UTILITY FUNCTIONS
+ ************************************************** */
+	
+	
+	
+
+	/* ********************************
+	 * 
+	 * 				Factorial
+	 * 				---------
+	 * 
+	 * 			1 * 2 * ... * n-1 * n
+	 * 
+	 *  				n!
+	 * 
+	 ******************************** */
+	public static long factorial(int n){
+		long nFactorial = 1;
+		
+		for (int i = n; i > 0; i--) {
+			nFactorial = nFactorial * i;
+		}
+		
+		return nFactorial;
+	}
+	
+	
+	
+	
+
+	/* *************************************
+	 * 																
+	 * 				Combination:
+	 * 				------------
+	 * 
+	 * 		Number of different combinations
+	 * 		you can make with r items out of
+	 * 			 n objects in a set.
+	 * 
+	 * 					/ n \
+	 * 					|	|
+	 * 					\ r /
+	 * 
+	 * 
+	 * 					 n!
+	 * 				= --------
+	 * 				  (n-r)!r!
+	 * 
+	 **************************************** */
+	
+	public static int combination(int n, int r){
+		
+		long nFactorial = factorial(n);
+		long nSubRFactorial = factorial(n-r);
+		long rFactorial = factorial(r);
+		
+		int C = (int) (nFactorial / (nSubRFactorial * rFactorial));
+		return C;
 		
 	}
 	
 	
 	
-	public static int subfactorial(int n){
+
+	/* ****************************************************
+	 * 	
+	 * 					Partial Derangement
+	 * 					-------------------
+	 * 
+	 * 			The number of combinations that can 
+	 * 			be made with k number of items in the 
+	 * 			wrong place out of a set of n items
+	 * 
+	 * http://mathworld.wolfram.com/PartialDerangement.html
+	 * 
+	 * 							/ n \
+	 * 				R(n, k) =	|	|	!(n-k)
+	 * 							\ k /
+	 * 
+	 *************************************************** */
+	
+	
+	public static long partialDerange(int n, int k) {
 		
-		/* From Wolfram.com:
-		 * 
-		 * http://mathworld.wolfram.com/Derangement.html
-		 * 
-		 * The function giving the number of distinct derangements on 
-		 * n elements is called the subfactorial !n and is equal to 
-		 * 
-		 * 				_n_	(-1)^k
-		 * 		!n=	 n! \	______
-		 *				/__	  k!
-		 *				k=0
-		 */
+		int combNK = combination(n, k);
+		//System.out.println("CombNK " + combNK);
+		
+		double subFactorialNK = subfactorial(n - k);
+		//System.out.println("sub fact of " + (n-k) + " is " + (int)subFactorialNK);
+		
+		int partialDer = (int)(combNK * subFactorialNK);
+		
+		//System.out.println("PartDir: " + partialDer);
+		
+		return partialDer;
+	}
+	
+	
+	
+	
+	/* *******************************************
+	 *  				Subfactorial:	
+	 * 
+	 * 						!n
+	 * 
+	 * from Wolfram.com:
+	 * http://mathworld.wolfram.com/Derangement.html
+	 * 
+	 * 		The function giving the number of distinct 
+	 * 		derangements on n elements is called the 
+	 * 		subfactorial !n and is equal to 
+	 * 
+	 * 						_n_	(-1)^i
+	 * 				!n=	 n! \	______
+	 *						/__	  i!
+	 *						i=0
+	 *
+	 ******************************************* */
+	
+	public static int subfactorial(int n){
 		
 		long nFactorial = factorial(n);
 		
@@ -49,79 +200,5 @@ public class DerangedExams {
 		
 		return (int) (nFactorial * sigma);
 	}
-	
-	
-	
-	public static long partialDerange(int n, int k) {
-		/* ****************************************************
-		 * http://mathworld.wolfram.com/PartialDerangement.html
-		 * 
-		 * 							/ n \
-		 * 				R(n, k) =	|	|	!(n-k)
-		 * 							\ k /
-		 * 
-		 *************************************************** */
-		
-		int combNK = combination(n, k);
-		//System.out.println("CombNK " + combNK);
-		
-		double subFactorialNK = subfactorial(n - k);
-		//System.out.println("sub fact of " + (n-k) + " is " + (int)subFactorialNK);
-		
-		int partialDer = (int)(combNK * subFactorialNK);
-		
-		//System.out.println("PartDir: " + partialDer);
-		
-		return partialDer;
-	}
-	
-	
-	
-	
-/* **************************************************
- * 					UTILITY FUNCTIONS
- ************************************************** */
-	
-	
-	/* ***********************************
-	 * 
-	 * Utility function for Factorial - n!
-	 * 
-	 ********************************** */
-	public static long factorial(int n){
-		long nFactorial = 1;
-		
-		for (int i = n; i > 0; i--) {
-			nFactorial = nFactorial * i;
-		}
-		
-		return nFactorial;
-	}
-	
-	
-	
-	
 
-	/* ******************************************
-	 * 										
-	 * Utility function for Combination:	/ n \
-	 * 										|	|
-	 * 										\ r /
-	 * 
-	 * 					 n!
-	 * 				= --------
-	 * 				  (n-r)!r!
-	 * 
-	 ******************************************* */
-	
-	public static int combination(int n, int r){
-		
-		long nFactorial = factorial(n);
-		long nSubRFactorial = factorial(n-r);
-		long rFactorial = factorial(r);
-		
-		int C = (int) (nFactorial / (nSubRFactorial * rFactorial));
-		return C;
-		
-	}
 }
